ddca32043b8f1ba8a67fd760a1a650e40fa31ba0
LUO Yihao <luo_y@worksap.co.jp>
2016-09-07 15:20:15 +0800

    [Simon LUO] Add base pom file.

---
+++ simoncat-front-build-base/src/main/java/com/simoncat/front/App.java
@@ -0,0 +1,13 @@
+package com.simoncat.front;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
