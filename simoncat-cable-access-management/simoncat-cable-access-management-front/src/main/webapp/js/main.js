1a91535bfe0408ae3a62f2b5666679c13a55dc4f
LUO Yihao <luo_y@worksap.co.jp>
2016-12-23 19:58:49 +0800

    [Simon LUO] Update the index page.

--- simoncat-cable-access-management/simoncat-cable-access-management-front/src/main/webapp/js/main.js
+++ simoncat-cable-access-management/simoncat-cable-access-management-front/src/main/webapp/js/main.js
@@ -23,58 +23,34 @@ $(document).ready(function() {
   $('[data-toggle="offcanvas"]').click(function() {
     $('#wrapper').toggleClass('toggled');
   });
-  
+
   initWorkTicketsTabs();
+  initWorkTicketsFloatMenu();
 });
 
 function initWorkTicketsTabs() {
-  $(".work_tickets_li").click(function(e) {
 
-    // make sure we cannot click the slider
-    if ($(this).hasClass('work_tickets_slider')) {
-      return;
+  // Hide all tab container
+  $(".work_tickets_container").hide();
+
+  $(".work_tickets_li:first").attr("id", "active");
+  $(".work_tickets_container:first").fadeIn();
+
+  $(".work_tickets_a").click(function(e) {
+    e.preventDefault();
+
+    var $closestLiElement = $(this).closest(".work_tickets_li");
+    if ($closestLiElement.attr("id") !== "active") {
+      $(".work_tickets_container").hide();
+      $(".work_tickets_li").attr("id", "");
+      $closestLiElement.attr("id", "active");
+      $('#' + $(this).attr('name')).fadeIn();
     }
+  });
+};
 
-    /* Add the slider movement */
-
-    // what tab was pressed
-    var whatTab = $(this).index();
-
-    // Work out how far the slider needs to go
-    var howFar = 160 * whatTab;
-
-    $(".work_tickets_slider").css({
-      left : howFar + "px"
-    });
-
-    /* Add the ripple */
-
-    // Remove olds ones
-    $(".ripple").remove();
-
-    // Setup
-    var posX = $(this).offset().left, posY = $(this).offset().top, buttonWidth = $(this).width(), buttonHeight = $(this).height();
-
-    // Add the element
-    $(this).prepend("<span class='ripple'></span>");
-
-    // Make it round!
-    if (buttonWidth >= buttonHeight) {
-      buttonHeight = buttonWidth;
-    } else {
-      buttonWidth = buttonHeight;
-    }
-
-    // Get the center of the element
-    var x = e.pageX - posX - buttonWidth / 2;
-    var y = e.pageY - posY - buttonHeight / 2;
-
-    // Add the ripples CSS and start the animation
-    $(".ripple").css({
-      width : buttonWidth,
-      height : buttonHeight,
-      top : y + 'px',
-      left : x + 'px'
-    }).addClass("rippleEffect");
+function initWorkTicketsFloatMenu() {
+  $(".work_tickets_float_menu_plus").click(function(e){
+    $(".work_tickets_float_menu").toggleClass("open")
   });
 }
